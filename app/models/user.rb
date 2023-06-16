5955b27e7cef895cfce26b8f6af8b0717d36878b
kitagawachihiro <ishikawachihiro0805@gmail.com>
2023-06-16 23:37:04 +0900

    genderにenum設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,6 +20,8 @@ class User < ApplicationRecord
 
   validates :email, uniqueness: true
 
+  enum gender: { その他: 0, 男性: 1, 女性: 2 }
+
   scope :allowing_created_event_notification,
         -> { joins(:notification_timings).merge(NotificationTiming.created_event) }
   scope :allowing_commented_to_event_notification,
